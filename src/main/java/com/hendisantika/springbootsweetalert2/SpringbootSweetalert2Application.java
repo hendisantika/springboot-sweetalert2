170dfb66e8b3f39c07e975878a63a6ef033da09e
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-24 10:50:22 +0700

    #2 - Add bootstrap

---
+++ src/main/java/com/hendisantika/springbootsweetalert2/SpringbootSweetalert2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsweetalert2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSweetalert2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSweetalert2Application.class, args);
+    }
+
+}
