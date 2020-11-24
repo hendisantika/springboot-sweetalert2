ba11b2b4404713aa711e5e787812feafe1cb19a3
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-24 11:40:03 +0700

    #9 - Add EmailContainer Entity

---
+++ src/main/java/com/hendisantika/springbootsweetalert2/entity/EmailContainer.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootsweetalert2.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sweetalert2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/11/20
+ * Time: 11.39
+ */
+@Data
+@NoArgsConstructor
+public class EmailContainer {
+    private String email;
+}
